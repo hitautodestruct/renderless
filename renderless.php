01bf49f5a6b9f7a788e8700c5fff43d4a0f1fd5f
netyou <yotam@netyou.co.il>
2013-04-27 18:02:33 +0300

    Added some comments to renderless.php

--- renderless.php
+++ renderless.php
@@ -2,12 +2,18 @@
 require 'lessc.inc.php';
 
 // Define the output file and the input directory
+// $input_dir: A relative path to your css/less files. The path should be relative to where the renderless.php file is located.
+// $output_file: If this variable is defined renderless will output a compiled css file. Again, this is relative to where renderless.php is located.
+// $pointer_file: You can specify a single file for renderless to render, the file can include @import statements and act as an organizer.
 $input_dir = 'css/';
 $output_file = '../style.css';
-$min = isset( $_REQUEST['min'] ) ? $_REQUEST['min'] : true;
-$pointer_file = isset( $_REQUEST['pointer'] ) ? $_REQUEST['pointer'] : false;
+$pointer_file = '';
 
-// For wordpress themes
+// min is useful for debugging and seeing the code unminified but compiled.
+// Simply append a min query with a falsy value like so: renderless.php?min=0
+$min = isset( $_REQUEST['min'] ) ? $_REQUEST['min'] : true;
+
+// Custom header
 $header = '/* This file was rendered using renderless.php v0.1 */';
 
 // Minifies css code
@@ -34,7 +40,7 @@ function file_type( $file_name ) {
 }
 
 // Get one file containing @import statements or get all files in the input_dir
-$files = $pointer_file ? array( $pointer_file ) : glob( $input_dir . '{*.css,*.less}', GLOB_BRACE);
+$files = $pointer_file != '' ? array( $pointer_file ) : glob( $input_dir . '{*.css,*.less}', GLOB_BRACE);
 
 $css = "";
 
